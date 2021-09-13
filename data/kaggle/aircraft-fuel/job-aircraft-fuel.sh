577dd5a9e674a132d08cb570284f4b2eee37ebfe
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-09-13 11:40:48 +0200

    Aircraft fuel results

---
+++ data/kaggle/aircraft-fuel/job-aircraft-fuel.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=alejandro.alvarezayllon@unige.ch
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+
+set -e
+
+eval "$(${HOME}/miniconda3/bin/conda shell.bash hook)"
+conda activate matchbox
+
+set -x
+
+MATCHDIR="${HOME}/MatchBox"
+ID="aircraft_fuel_$(date +%Y%m%d)"
+mkdir -p "${MATCHDIR}/results/$ID"
+
+for i in $(seq 1 $SLURM_NTASKS); do
+    srun --exclusive -N1 -n1 "${MATCHDIR}/bin/benchmark.py" \
+        --id "${ID}" \
+        --repeat 1000 \
+        --output-dir "${MATCHDIR}/results/" \
+        --sample-size 50 \
+        ${MATCHDIR}/data/kaggle/aircraft-fuel/*.csv &> "${MATCHDIR}/results/$ID/run.${i}.log" &
+done
+
+wait
+
